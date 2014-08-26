27fd14d45ca42c4ecdc6ed7c17c18161bea65c7f
Jeff C <jconway@arcwebtech.com>
2014-08-26 16:10:05 -0400

    Count total user hours

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
   has_many :subjects_learning, :through => :consuming_enrollments, :source => :subject
 
   has_many :tutor_applications, dependent: :destroy
+  has_many :requests, dependent: :destroy
 
   def incoming_tutor_id
     return self.id
@@ -19,6 +20,24 @@ class User < ActiveRecord::Base
 
   def display_name
     self.first_name + " " + self.last_name
+  end
+
+  def total_tutoring_hours
+    requests = Request.where(tutor_id: self.id)
+    
+    if requests.count == 0
+      return 0
+    end
+
+    completed_requests = []
+    total_hours = 0
+    requests.each do |r|
+      if r.status == "Accepted" and r.session_review.date != nil
+        total_hours += r.duration
+      end
+    end
+
+    return total_hours
   end
   
 end
