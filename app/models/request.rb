b0aed950c02216763c82ad8c70693b4517f78901
Jeff C <jconway@arcwebtech.com>
2014-09-08 09:22:39 -0400

    Requires a subject ID

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,7 @@ class Request < ActiveRecord::Base
   belongs_to :learner, class_name: "User"
   belongs_to :tutor, class_name: "User"
   belongs_to :subject
+  validates :subject_id, presence: true
   has_one :session_review, dependent: :destroy
 
   #POSSIBLE STATUSES: REQUESTED, ACCEPTED, REJECTED, NEEDS SURVEY, COMPLETED
